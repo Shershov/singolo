957e1feb2dba43a1344dd39b39dbcd7900e5c48d
Eugene Shershov <Absolute@tut.by>
2020-03-15 16:17:20 +0300

    fix: add links

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
 // heder nav
 
-const NAVI_BLOCK = document.querySelectorAll('.header__nav')[0];
-
-NAVI_BLOCK.addEventListener('click', addStyleToNavigation, false);
-
-function addStyleToNavigation(event) {
-    if (event.target.tagName !== 'A') return;
-    else {
-        NAVI_BLOCK.querySelectorAll('a').forEach(elem => elem.classList.remove('current__link'));
-        event.target.classList.add('current__link');
+let menuElements = document.querySelectorAll(".navigation > li > a");
+for (element of menuElements) {
+    element.onclick = function(e) {
+        e.target.classList.add('current__link');
+        for (element of menuElements) {
+            if (element != e.target) element.classList.remove('current__link');
+        }
     }
 }
+console.log(menuElements);
 
 // slider
 const btnPrev = document.querySelector('.phone_inner .left__arrow');
@@ -19,7 +18,7 @@ const btnNext = document.querySelector('.phone_inner .right__arrow');
 let images = document.querySelectorAll('.phone_inner .slide__field');
 
 let i = 0;
-console.log(images);
+
 
 btnPrev.onclick = function() {
     images[i].style.display = 'none';
