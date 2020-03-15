02a14d466b0eae1547d8a9e2747ec625ed85e81f
Eugene Shershov <Absolute@tut.by>
2020-03-15 10:27:07 +0300

    feat: add slider

--- script.js
+++ script.js
@@ -11,3 +11,34 @@ function addStyleToNavigation(event) {
         event.target.classList.add('current__link');
     }
 }
+
+// slider
+const btnPrev = document.querySelector('.phone_inner .left__arrow');
+const btnNext = document.querySelector('.phone_inner .right__arrow');
+
+let images = document.querySelectorAll('.phone_inner .slide__field');
+
+let i = 0;
+console.log(images);
+
+btnPrev.onclick = function() {
+    images[i].style.display = 'none';
+    i--;
+
+    if (i < 0) {
+        i = images.length - 1;
+    }
+
+    images[i].style.display = 'flex';
+}
+
+btnNext.onclick = function() {
+    images[i].style.display = 'none';
+    i++;
+
+    if (i >= images.length) {
+        i = 0;
+    }
+
+    images[i].style.display = 'flex';
+}
