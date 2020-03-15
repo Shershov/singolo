217da0f6fd26030a88f70fec2cbbe495886aa0db
Eugene Shershov <Absolute@tut.by>
2020-03-15 19:49:17 +0300

    feat: add mobile screens

--- script.js
+++ script.js
@@ -1,15 +1,11 @@
 // heder nav
 
-let menuElements = document.querySelectorAll(".navigation > li > a");
-for (element of menuElements) {
-    element.onclick = function(e) {
-        e.target.classList.add('current__link');
-        for (element of menuElements) {
-            if (element != e.target) element.classList.remove('current__link');
-        }
-    }
-}
-console.log(menuElements);
+const navMenu = document.getElementById('nav__menu');
+navMenu.addEventListener('click', (event) => {
+    navMenu.querySelectorAll('li > a').forEach(el => el.classList.remove('current__link'));
+    event.target.classList.add('current__link');
+})
+
 
 // slider
 const btnPrev = document.querySelector('.phone_inner .left__arrow');
@@ -41,3 +37,30 @@ btnNext.onclick = function() {
 
     images[i].style.display = 'flex';
 }
+
+// slider screens
+
+
+const iphones = document.querySelectorAll(".iphon-vert > img");
+const mobile = document.querySelector(".mobile");
+let j = 0;
+
+mobile.onclick = function() {
+    iphones[j].style.display = 'none';
+    j++;
+    if (j >= iphones.length) {
+        j = 0;
+    }
+    iphones[j].style.display = 'block';
+}
+console.log(iphones);
+
+
+
+// portfolio
+
+const portfolioMenu = document.getElementById('portfolio__menu');
+portfolioMenu.addEventListener('click', (event) => {
+    portfolioMenu.querySelectorAll('li').forEach(el => el.classList.remove('portfolio__active-link'));
+    event.target.classList.add('portfolio__active-link');
+})
