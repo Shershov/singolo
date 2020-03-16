bf7ae8c28fbaf39ad04c3f479f5d7d74248c065d
Eugene Shershov <Absolute@tut.by>
2020-03-16 12:05:30 +0300

    feat: add portfolio script

--- script.js
+++ script.js
@@ -10,31 +10,24 @@ navMenu.addEventListener('click', (event) => {
 // slider
 const btnPrev = document.querySelector('.phone_inner .left__arrow');
 const btnNext = document.querySelector('.phone_inner .right__arrow');
-
 let images = document.querySelectorAll('.phone_inner .slide__field');
-
 let i = 0;
-
 
 btnPrev.onclick = function() {
     images[i].style.display = 'none';
     i--;
-
     if (i < 0) {
         i = images.length - 1;
     }
-
     images[i].style.display = 'flex';
 }
 
 btnNext.onclick = function() {
     images[i].style.display = 'none';
     i++;
-
     if (i >= images.length) {
         i = 0;
     }
-
     images[i].style.display = 'flex';
 }
 
@@ -42,10 +35,10 @@ btnNext.onclick = function() {
 
 
 const iphones = document.querySelectorAll(".iphon-vert > img");
-const mobile = document.querySelector(".mobile");
+const mobileBtnVert = document.querySelector(".mobile__btn-vert");
 let j = 0;
 
-mobile.onclick = function() {
+mobileBtnVert.onclick = function() {
     iphones[j].style.display = 'none';
     j++;
     if (j >= iphones.length) {
@@ -53,14 +46,39 @@ mobile.onclick = function() {
     }
     iphones[j].style.display = 'block';
 }
-console.log(iphones);
+
+const iphones2 = document.querySelectorAll(".iphon-horiz > img");
+const mobileBtnHor = document.querySelector(".mobile__btn-hor");
+let k = 0;
+
+mobileBtnHor.onclick = function() {
+    iphones2[k].style.display = 'none';
+    k++;
+    if (k >= iphones2.length) {
+        k = 0;
+    }
+    iphones2[k].style.display = 'block';
+}
 
 
 
-// portfolio
+// portfolio menu
 
-const portfolioMenu = document.getElementById('portfolio__menu');
-portfolioMenu.addEventListener('click', (event) => {
-    portfolioMenu.querySelectorAll('li').forEach(el => el.classList.remove('portfolio__active-link'));
-    event.target.classList.add('portfolio__active-link');
-})
+const portfolioTabs = document.querySelectorAll('#portfolio__menu > li');
+const portfolioImgItems = document.querySelectorAll('.porfolio__item');
+for (let element of portfolioTabs) {
+    element.onclick = function(e) {
+        e.target.classList.add('portfolio__active-link');
+        for (let element of portfolioTabs) {
+            if (element != e.target) element.classList.remove('portfolio__active-link');
+        }
+    }
+}
+// portfolio items
+for (let portfolioTab of portfolioTabs) {
+    portfolioTab.onclick = function() {
+        for (let portfolioImgItem of portfolioImgItems) {
+            portfolioImgItem.style.order = Math.floor((Math.random() * 12));
+        }
+    }
+}
