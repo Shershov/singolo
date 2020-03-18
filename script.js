c004e66b90222787b7b9d3c50201f52b95448164
Eugene <absolute@tut.by>
2020-03-18 20:55:12 +0300

    fix: add correct portfolio tabs

--- script.js
+++ script.js
@@ -97,23 +97,24 @@ function showBgForward() {
 
 
 
-// portfolio menu
+// portfolio menu && images
 for (let element of portfolioTabs) {
     element.onclick = function(e) {
         e.target.classList.add('portfolio__active-link');
+        for (let portfolioImgItem of portfolioImgItems) {
+            portfolioImgItem.style.order = Math.floor((Math.random() * 12));
+        }
         for (let element of portfolioTabs) {
             if (element != e.target) element.classList.remove('portfolio__active-link');
         }
     }
 }
-// portfolio images
-for (let portfolioTab of portfolioTabs) {
-    portfolioTab.onclick = function() {
-        for (let portfolioImgItem of portfolioImgItems) {
-            portfolioImgItem.style.order = Math.floor((Math.random() * 12));
-        }
-    }
-}
+// portfolio 
+
+    
+        
+    
+
 // portfolio bordered
 
 portfolioItems.addEventListener('click', (e) => {
